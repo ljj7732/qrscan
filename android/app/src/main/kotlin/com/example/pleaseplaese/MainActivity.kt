8921ae3f0bd98a2423196e39048becd49b60911b
이종진 <ljj7732@naver.com>
2023-01-10 16:54:49 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/pleaseplaese/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pleaseplaese
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String _scanBarcode = 'Unknown';
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+
+
+  Future<void> scanQR() async {
+    String barcodeScanRes;
+    try {
+      barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+          '#ff6666', 'Cancel', true, ScanMode.QR);
+      print(barcodeScanRes);
+    } on PlatformException {
+      barcodeScanRes = 'Failed to get platform version.';
+    }
+//barcode scanner flutter ant
+    setState(() {
+      _scanBarcode = barcodeScanRes;
+    });
+  }
+
+
+//barcode scanner flutter ant
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            appBar: AppBar(title: const Text('Barcode Scanner')),
+            body: Builder(builder: (BuildContext context) {
+              return Container(
+                  alignment: Alignment.center,
+                  child: Flex(
+                      direction: Axis.vertical,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        ElevatedButton(
+                            onPressed: () => scanQR(),
+                            child: const Text('QR scan')),
+                        Text('Scan result : $_scanBarcode\n',
+                            style: const TextStyle(fontSize: 20))
+                      ]));
+            })));
+  }
+}
